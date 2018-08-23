2ae0d8a6ecbf20b1a040e160f769e8874d5b074c
vineet <vinit2951996@gmail.com>
2018-08-23 21:48:50 +0530

    updated

--- app/routes.py
+++ app/routes.py
@@ -1,21 +1,35 @@
+from flask import render_template,redirect
 from app import app,db,links
-from flask import request
+from flask import Flask,request, url_for,jsonify
+
+
 
 @app.route('/')
-@app.before_request
-def index():
-    path = request.path
-    x = path[1:]
-    print(x)
-
+@app.route('/urlinfo/1/<path:arg>')
+def home(arg):
     lk = links.query.order_by(links.url).all()
 
     op = list()
     for i in lk:
         op.append(i.url)
 
-    # print(op)
-    if x in op:
-        return "False"
+    if arg in op:
+        return jsonify({'value':arg ,'success':False})
     else:
-        return "True"
+        return jsonify({'value':arg ,'success':True})
+
+@app.route('/add/<path:arg2>')
+def add(arg2):
+    lk1 = links.query.order_by(links.url).all()
+
+    op1 = list()
+    for i in lk1:
+        op1.append(i.url)
+
+    if arg2 in op1:
+        return jsonify({'value':'url already present in database','success':False})
+    else:
+        me =  links(url=arg2)
+        db.session.add(me)
+        db.session.commit()
+        return jsonify({'value':'url added to database','success':True})
